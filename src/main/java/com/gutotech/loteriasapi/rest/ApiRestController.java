08fbdb2fc968dbae95cf598d8e9b2e66ffb5f5aa
geilsonfreire <geilosnfreireleite@gmail.com>
2025-02-13 23:34:20 -0300

    Atualiza a descrição da tag da API e habilita a documentação do Swagger

--- src/main/java/com/gutotech/loteriasapi/rest/ApiRestController.java
+++ src/main/java/com/gutotech/loteriasapi/rest/ApiRestController.java
@@ -20,28 +20,27 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "API Loterias")
+@Tag(name = "Loterias", description = "Endpoints para consultar resultados de loterias")
 public class ApiRestController {
 
     private final List<String> lotteries = Loteria.asList();
-
-    private final String invalidLotteryMessageFormat = "'%s' não é o id de nenhuma das loterias suportadas. Loterias suportadas: "
-            + lotteries;
-
-    @Autowired
-    private ResultadoService resultadoService;
-
-    @GetMapping
-    @Operation(summary = "Retorna todas as loterias disponíveis para pesquisa.")
-    public ResponseEntity<List<String>> getLotteries() {
-        return ResponseEntity.ok(Loteria.asList());
-    }
-
-    @GetMapping("{loteria}")
-    @Operation(summary = "Retorna todos os resultados já realizados da loteria especificada.")
-    public ResponseEntity<List<Resultado>> getResultsByLottery(
-            @Parameter(description = "Nome da loteria", example = "megasena", required = true) @PathVariable("loteria") String loteria) {
-
+    
+        private final String invalidLotteryMessageFormat = "'%s' não é o id de nenhuma das loterias suportadas. Loterias suportadas: "
+                + lotteries;
+    
+        @Autowired
+        private ResultadoService resultadoService;
+    
+        @GetMapping
+        @Operation(summary = "Retorna todas as loterias disponíveis para pesquisa.")
+        public ResponseEntity<List<String>> getLotteries() {
+            return ResponseEntity.ok(Loteria.asList());
+        }
+    
+        @GetMapping("{loteria}")
+        @Operation(summary = "Retorna todos os resultados já realizados da loteria especificada.")
+        public ResponseEntity<List<Resultado>> getResultsByLottery(
+                @Parameter(description = "Nome da loteria", example = "megasena", required = true) @PathVariable("loteria") String loteria) {
         if (!lotteries.contains(loteria)) {
             throw new ResourceNotFoundException(String.format(invalidLotteryMessageFormat, loteria));
         }
