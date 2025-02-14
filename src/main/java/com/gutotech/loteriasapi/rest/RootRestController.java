383946b0065aa52f824a12fa1e6c56fe89299714
geilsonfreire <geilosnfreireleite@gmail.com>
2025-02-13 22:46:46 -0300

    Foi removido a duplicação de endpoints do swagger!

--- src/main/java/com/gutotech/loteriasapi/rest/RootRestController.java
+++ src/main/java/com/gutotech/loteriasapi/rest/RootRestController.java
@@ -1,74 +1,17 @@
 package com.gutotech.loteriasapi.rest;
 
-import java.util.List;
+import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gutotech.loteriasapi.model.Loteria;
-import com.gutotech.loteriasapi.model.Resultado;
-import com.gutotech.loteriasapi.model.exception.ResourceNotFoundException;
-import com.gutotech.loteriasapi.service.ResultadoService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 @RestController
 @RequestMapping("/")
-@Tag(name = "Loterias")
 public class RootRestController {
 
-    private final List<String> lotteries = Loteria.asList();
-
-    private final String invalidLotteryMessageFormat = "'%s' não é o id de nenhuma das loterias suportadas. Loterias suportadas: "
-            + lotteries;
-
-    @Autowired
-    private ResultadoService resultadoService;
-
-    @GetMapping
-    @Operation(summary = "Retorna todas as loterias disponíveis para pesquisa.")
-    public ResponseEntity<List<String>> getLotteries() {
-        return ResponseEntity.ok(Loteria.asList());
+    @RequestMapping(value = "/")
+    public void redirectToSwagger(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/");
     }
-
-    @GetMapping("{loteria}")
-    @Operation(summary = "Retorna todos os resultados já realizados da loteria especificada.")
-    public ResponseEntity<List<Resultado>> getResultsByLottery(
-            @Parameter(description = "Nome da loteria", example = "megasena") @PathVariable("loteria") String loteria) {
-        if (!lotteries.contains(loteria)) {
-            throw new ResourceNotFoundException(String.format(invalidLotteryMessageFormat, loteria));
-        }
-
-        return ResponseEntity.ok(resultadoService.findByLoteria(loteria));
-    }
-
-    @GetMapping("{loteria}/{concurso}")
-    @Operation(summary = "Retorna o resultado da loteria e concurso especificado.")
-    public ResponseEntity<Resultado> getResultById(
-            @Parameter(description = "Nome da loteria", example = "megasena") @PathVariable("loteria") String loteria,
-            @Parameter(description = "Número do concurso") @PathVariable("concurso") Integer concurso) {
-        if (!lotteries.contains(loteria)) {
-            throw new ResourceNotFoundException(String.format(invalidLotteryMessageFormat, loteria));
-        }
-
-        return ResponseEntity.ok(resultadoService.findByLoteriaAndConcurso(loteria, concurso));
-    }
-
-    @GetMapping("{loteria}/latest")
-    @Operation(summary = "Retorna o resultado mais recente da loteria especificada.")
-    public ResponseEntity<Resultado> getLatestResult(
-            @Parameter(description = "Nome da loteria", example = "megasena") @PathVariable("loteria") String loteria) {
-        if (!lotteries.contains(loteria)) {
-            throw new ResourceNotFoundException(String.format(invalidLotteryMessageFormat, loteria));
-        }
-
-        return ResponseEntity.ok(resultadoService.findLatest(loteria));
-    }
-
 }
